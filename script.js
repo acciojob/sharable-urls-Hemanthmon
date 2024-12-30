222ca06063c00b77d013c0f5c78650a6ba26bb10
Hemanth V <153289923+Hemanthmon@users.noreply.github.com>
2024-12-30 12:53:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // your code here
+const nameInput = document.getElementById('name')
+const yearInput = document.getElementById('year')
+const url = document.getElementById('url')
+const button = document.getElementById('button')
+
+
+//update the url when the button is clicked
+button.onclick = function() {
+    const name = nameInput.value.trim()
+    const year = yearInput.value.trim()
+
+    url.textContent = `https://localhost:8080/?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`
+}
